a55d37fba3da3cfda7fa5dea99890df88c72fc5d
Gaurav Amol Bijwe <gabijwe@gcoen.ac.in>
2025-05-12 20:33:54 +0530

    fixed overlay video in browser

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def overlay_video(input_video_path, json_path, output_video_path):
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
     out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
     
     if inference_data.get("status_info") == "decode error":
